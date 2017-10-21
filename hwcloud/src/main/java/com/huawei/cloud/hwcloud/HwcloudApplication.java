0925bd9046f4f5c302f72157bce3370e26f95270
rml <myrml@163.com>
2017-10-21 13:54:08 +0800

    new project

---
+++ hwcloud/src/main/java/com/huawei/cloud/hwcloud/HwcloudApplication.java
@@ -0,0 +1,12 @@
+package com.huawei.cloud.hwcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwcloudApplication.class, args);
+	}
+}
